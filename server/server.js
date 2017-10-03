9e192b24a2c64448f9f87ff22e37a5d91a49bdc7
Jayz Walker <>
2017-10-03 14:32:22 +0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jayzwalker on 10/3/17.
+ */
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
